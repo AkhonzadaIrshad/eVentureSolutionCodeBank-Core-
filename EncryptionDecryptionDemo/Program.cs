4c2e4fd8c1bb92d14c4d97fe6f70860c98a0676c
Irshad Ahmed <Irshadahmed31@outlook.com>
2017-06-01 20:24:46 +0500

    Encryption Decryption Demo

---
+++ EncryptionDecryptionDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace EncryptionDecryptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data=string.Empty;
+            int key;
+            string encryptedForm = string.Empty;
+            string decryptedFrom = string.Empty;
+            Console.WriteLine("Enter Data To Encrypt");
+            data = Console.ReadLine();
+            Console.WriteLine("Enter Encryption Key");
+            key = Convert.ToInt32(Console.ReadLine());
+            foreach (char character in data.ToCharArray())
+            {
+                encryptedForm += Convert.ToChar(Convert.ToInt32(character + key));
+                encryptedForm += ' ';
+            }
+            Console.WriteLine($"Encrypted Form {encryptedForm}");
+            foreach (string character in encryptedForm.ToString().Split(' '))
+            {
+                if (character != "")
+                {
+                    decryptedFrom += Convert.ToChar(Convert.ToInt32(Convert.ToChar(character)) - key);
+                }
+            }
+            Console.WriteLine($"Decrypted Form {decryptedFrom}");
+            Console.ReadKey();
+        }
+    }
+}
