8a2efc16922ee6d9b1db17b26621144744774a4d
Irshad Ahmed <Irshadahmed31@outlook.com>
2017-07-02 19:30:00 +0500

    Searching a key word in a paragraph with Ignoring case

---
+++ SearchStringInParagraphDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SearchStringInParagraphDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Paragraph");
+            string paragraph =Console.ReadLine();
+            Console.WriteLine("Enter a Keyword to search");
+            string keyword =Console.ReadLine();
+
+            string[] values = paragraph.Split(' ');
+            Console.Clear();
+            for (int i=0;i<values.Length;i++)
+            {
+                if (values[i].Equals(keyword,StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Keyword {values[i]} found at index {i}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
