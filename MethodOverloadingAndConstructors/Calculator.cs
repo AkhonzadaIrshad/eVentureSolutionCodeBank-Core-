7c4b328609508123e799e65b5f4cf7b0ce440829
Irshad Ahmed <Irshadahmed31@outlook.com>
2017-07-08 19:27:52 +0500

    dotNET 312

---
+++ MethodOverloadingAndConstructors/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverloadingAndConstructors
+{
+    class Calculator
+    {
+        static Calculator()
+        {
+            pi = 3.14f;
+        }
+   
+        private static float pi;
+
+        public static float AreaOfCircle(float radius)
+        {
+            return radius * radius * pi;
+        }
+
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static string Add(string a, string b)
+        {
+            return a + b;
+        }
+        public static int Add(int a, int b,int c)
+        {
+            return a + b;
+        }
+        public static int Add(int a, float b)
+        {
+            return a + (int)b;
+        }
+        public static int Add(float a, int b)
+        {
+            return (int)a + b;
+        }
+    }
+}
