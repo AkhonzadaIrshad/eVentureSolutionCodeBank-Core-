6cae02db72e5f8639e582c576bd8124828ed2505
Irshad Ahmed <Irshadahmed31@outlook.com>
2017-08-05 21:41:01 +0500

    Saturday August 5,2017

---
+++ SecondDayWithMethods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondDayWithMethods
+{
+    class Program
+    {
+       static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static bool AreEqual(int a, int b)
+        {
+            return a==b?true:false;
+        }
+
+
+        static void Main(string[] args)
+        {
+          //int c= Addition(2,4)+100;
+           Calculator calculator=new Calculator();
+            string valueOne = "This is Evs Lahore";
+            string ValueTwo = "This is Another EVS";
+            calculator.PrintMessage(valueOne,ValueTwo);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Calculator
+    {
+        public void PrintMessage(string TextOne,string TextTwo)
+        {
+            if (TextOne.Equals("EVS"))
+            {
+                Console.WriteLine($"Welcome To {TextOne} Lahore");
+            }
+            else if (TextOne.Contains("Evs"))
+            {
+                Console.WriteLine($"Message {TextOne}");
+
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        
+
+    }
+}
